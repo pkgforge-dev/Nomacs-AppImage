91b5c6637efd6d70c55466808f2234ab378b40c5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-16 21:25:46 -0400

    add `qt6-imageformats`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm qt6-imageformats
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
